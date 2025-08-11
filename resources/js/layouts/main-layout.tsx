54ea7565cd8fdc637d16d9762f4492295cc5e41e
Mirco Baalmans <mircobaalmans@live.nl>
2025-08-11 13:04:03 +0200

    Add: Hoofdpagina ontwerp af

---
+++ resources/js/layouts/main-layout.tsx
@@ -0,0 +1,95 @@
+import { NavItem } from '@/types';
+import { Link } from '@inertiajs/react';
+import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from '@radix-ui/react-navigation-menu';
+import { Copyright } from 'lucide-react';
+import {type ReactNode} from 'react';
+
+interface AppLayoutProps {
+    children: ReactNode;
+}
+
+const navItems:NavItem[] = [
+    {
+        title: "Home",
+        href: "/",
+        isActive: true,
+    },
+    {
+        title: "Wordpress",
+        href: "/wordpress",
+    },
+    {
+        title: "Laravel",
+        href: "/laravel",
+    },
+    {
+        title: "Over mij",
+        href: "/over-mij",
+    },
+    {
+        title: "Contact",
+        href: "/contact",
+    }
+]
+
+export default function MainLayout( {children, ...props}: AppLayoutProps) {
+    
+    return (
+    <div className='bg-dark' { ...props }>
+    <header>
+        <NavigationMenu className='py-5 mb-5 shadow bg-dark-bg'>
+            <NavigationMenuList className='flex flex-row justify-between max-w-screen-xl mx-auto'>
+                {navItems.map((item,index) => (
+                    item.isActive ? (
+                        <NavigationMenuItem key={index} className='rounded border-b-2 border-primary even:border-accent shadow-xl p-2 text-center bg-primary-hover even:bg-accent-hover text-white '>
+                            <Link href={item.href}>
+                                {item.title}
+                            </Link>
+                        </NavigationMenuItem>
+                    ):(
+                        <NavigationMenuItem key={index} className='rounded border-b-2 border-primary even:border-accent shadow-xl p-2 bg-white  text-center hover:bg-primary-hover hover:even:bg-accent-hover hover:text-white '>
+                            <Link href={item.href}>
+                                {item.title}
+                            </Link>
+                        </NavigationMenuItem>
+                    )
+                ))}
+            </NavigationMenuList>
+        </NavigationMenu>
+    </header>
+    <main>
+        {children}
+    </main>
+    <footer className='bg-dark-bg text-light min-h-10 p-2'>
+        {/* <div className='max-w-screen-xl mx-auto py-5 flex flex-row gap-5 justify-between'>
+             <NavigationMenu className='py-5 mb-5 shadow bg-dark-bg'>
+                <NavigationMenuList className='flex flex-col gap-2 justify-between max-w-screen-xl mx-auto'>
+                    {navItems.map((item,index) => (
+                        item.isActive ? (
+                            <NavigationMenuItem key={index} className='underline decoration-primary decoration-2'>
+                                <Link href={item.href}>
+                                    {item.title}
+                                </Link>
+                            </NavigationMenuItem>
+                        ):(
+                            <NavigationMenuItem key={index} className='underline decoration-accent decoration-2 '>
+                                <Link href={item.href}>
+                                    {item.title}
+                                </Link>
+                            </NavigationMenuItem>
+                        )
+                    ))}
+                </NavigationMenuList>
+            </NavigationMenu>
+
+            <ul>
+                <li><strong>Contact</strong></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div> */}
+        <span className='flex flex-row gap-2 text-muted'><Copyright /> Mirco Baalmans 2025</span>
+    </footer>
+    </div>
+    );
+}
